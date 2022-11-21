abc9930efad985c9e8e7edf672efe7db6796664c
eminakcil <eminakcil@outlook.com>
2022-11-21 19:38:29 +0300

    login page design

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,4 +1,32 @@
+import Input from '@components/Input'
+
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <div className="min-h-screen bg-gray-800 text-gray-300 grid place-items-center">
+      <div className="p-4 bg-gray-700 rounded-2xl border-2 border-solid border-gray-900 max-w-lg w-full">
+        <div className="flex flex-col gap-3">
+          <span className="font-medium text-xl">Giriş Yap</span>
+          <form>
+            <div className="space-y-3">
+              <div>
+                <Input label="E-posta Adresi" />
+              </div>
+              <div>
+                <Input label="Şifre" />
+              </div>
+              <div className="text-end">
+                <button
+                  type="submit"
+                  className="rounded-xl bg-gray-800 hover:bg-gray-700 border-2 border-solid border-gray-800 px-5 py-2"
+                >
+                  Giriş
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default Login
