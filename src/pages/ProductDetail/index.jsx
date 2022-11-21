9f355db84156017240039164dc0da002119ae77a
eminakcil <eminakcil@outlook.com>
2022-11-21 22:39:40 +0300

    product detail page fetch

--- src/pages/ProductDetail/index.jsx
+++ src/pages/ProductDetail/index.jsx
@@ -1,25 +1,26 @@
-import { useMemo } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import categories from '../../categories.json'
 import { getPath } from '../../utils'
 import LazyImage from '../../components/LazyImage'
+import { ProductService } from '@/services'
+import SpinnerLoader from '@components/SpinnerLoader'
 
 export default function ProductDetail() {
   const { id } = useParams()
 
-  const product = useMemo(
-    () =>
-      categories
-        .map((x) => x.products)
-        .flat()
-        .find((product) => product.id === id),
-    [id]
-  )
+  const [product, setProduct] = useState(false)
+  const [loading, setLoading] = useState(true)
 
-  const category = useMemo(
-    () => categories.find((x) => x.products.map((p) => p.id).includes(id)),
-    [product]
-  )
+  const fetchData = useCallback(() => {
+    ProductService.getById(id)
+      .then((response) => setProduct(response))
+      .finally(() => setLoading(false))
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  if (loading) return <SpinnerLoader />
 
   return (
     <div className="min-h-screen flex flex-col gap-4">
@@ -27,20 +28,20 @@ export default function ProductDetail() {
         <div>
           <LazyImage
             className="absolute top-0 left-0 w-full h-full object-fill rounded-b-xl"
-            src={product.image}
+            src={product.photo}
           />
         </div>
       </div>
 
       <div className="px-4">
-        <Link to={getPath('categoryDetail', { id: category.id })}>
-          <span className="flex gap-2">{category.name}</span>
+        <Link to={getPath('categoryDetail', { id: product.category._id })}>
+          <span className="flex gap-2">{product.category.title}</span>
         </Link>
       </div>
 
       <div className="flex-1 px-4 mb-4 flex flex-col">
         <div className="flex justify-between items-end">
-          <span className="font-medium text-xl">{product.name}</span>
+          <span className="font-medium text-xl">{product.title}</span>
           <span className="text-2xl">{`${product.price.toFixed(2)}`.replace('.', ',')} TL</span>
         </div>
         {product?.description && <div>{product.description}</div>}
