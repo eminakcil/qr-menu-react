a1b48fdb4dd19ea75b11e11491082e1e430da076
eminakcil <eminakcil@outlook.com>
2022-11-21 19:56:37 +0300

    toast container

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { Toaster } from 'react-hot-toast'
 import { useRoutes } from 'react-router-dom'
 import routes from './routes'
 
 const App = () => {
-  return useRoutes(routes)
+  return (
+    <>
+      {useRoutes(routes)}
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          style: {
+            borderRadius: '10px',
+            background: '#333',
+            color: '#fff',
+          },
+        }}
+      />
+    </>
+  )
 }
 export default App
