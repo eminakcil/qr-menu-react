f431da2992ae3d107743818f332658d18cb08197
eminakcil <eminakcil@outlook.com>
2022-12-04 23:57:55 +0300

    token rotation (experimental)

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -3,3 +3,4 @@ import { postJSON } from './request'
 const SERVICE_PATH = 'users'
 
 export const login = (data) => postJSON(SERVICE_PATH + '/login', data)
+export const refreshToken = (data) => postJSON(SERVICE_PATH + '/refresh-token', data)
--- src/services/request.js
+++ src/services/request.js
@@ -1,5 +1,6 @@
 import store from '../store'
 import { setUser } from '../store/authSlice'
+import { AuthService } from './index'
 
 function parseData(data) {
   const formData = new FormData()
@@ -16,44 +17,61 @@ function request(url, data = false, method = 'GET', type = 'FORM_DATA') {
       headers: {},
     }
 
-    const {
-      auth: { user },
-    } = store.getState()
+    const fetchData = (inner = false) => {
+      const {
+        auth: { user },
+      } = store.getState()
 
-    if (user) {
-      options.headers.Authorization = ['Bearer', user.tokens.accessToken].join(' ')
-    }
+      if (user) {
+        options.headers.Authorization = ['Bearer', user.tokens.accessToken].join(' ')
+      }
 
-    if (data) {
-      if (type === 'JSON') options.headers['Content-Type'] = 'application/json'
-      options.body = type === 'JSON' ? JSON.stringify(data) : parseData(data)
-    }
+      if (data) {
+        if (type === 'JSON') options.headers['Content-Type'] = 'application/json'
+        options.body = type === 'JSON' ? JSON.stringify(data) : parseData(data)
+      }
 
-    fetch(import.meta.env.VITE_API_URL + url, options)
-      .then(async (response) => {
-        const result = await response.json()
-        if (response.ok) {
-          resolve(result)
-        } else {
-          if ('message' in result) {
-            switch (result.message) {
-              case 'not logged in':
-                // refresh token and login request :)
-                if (user) {
+      fetch(import.meta.env.VITE_API_URL + url, options)
+        .then(async (response) => {
+          const result = await response.json()
+
+          if (response.ok) {
+            resolve(result)
+          } else {
+            if ('error' in result) {
+              switch (result.error.message) {
+                case 'not logged in':
                   store.dispatch(setUser(false))
+                  break
+                case 'jwt expired': {
+                  if (!inner) {
+                    AuthService.refreshToken({
+                      refreshToken: user.tokens.refreshToken,
+                    })
+                      .then((response) => {
+                        store.dispatch(setUser({ ...user, tokens: response }))
+                        fetchData(true)
+                      })
+                      .catch((error) => {
+                        reject(error)
+                      })
+                  }
+                  return
                 }
-                break
 
-              default:
-                break
+                default:
+                  break
+              }
             }
+            reject(result)
           }
-          reject(result)
-        }
-      })
-      .catch((error) => {
-        reject(error)
-      })
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    }
+
+    fetchData()
   })
 }
 
