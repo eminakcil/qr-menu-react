1b82cb00becabf7e53fc24f76f5b233fed98911e
eminakcil <eminakcil@outlook.com>
2022-12-05 00:05:48 +0300

    call ping on create in admin layout

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -2,8 +2,14 @@ import { Outlet } from 'react-router-dom'
 import Header from '@components/admin/Header'
 import Sidebar from '@components/admin/Sidebar'
 import Footer from '@components/admin/Footer'
+import { useEffect } from 'react'
+import { GeneralService } from '@/services'
 
 const AdminLayout = () => {
+  useEffect(() => {
+    GeneralService.ping()
+  }, [])
+
   return (
     <div className="min-h-screen flex flex-col bg-gray-800 text-gray-300">
       <Header />
--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,5 @@
 export * as CategoryService from './Categories'
 export * as ProductService from './Products'
 export * as AuthService from './Auth'
+
+export * as GeneralService from './General'
